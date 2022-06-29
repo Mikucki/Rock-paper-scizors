627c90f1cd41ed1f1f959376876e19f81b099612
Mikucki <piotr.mikucki@op.pl>
2022-06-29 12:07:57 -0400

    finish the game of rock paper scisors

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
 
-let pick = ['rock', 'paper', 'scisors'];
-
+function playerChoice() {
+    let playerSelection = prompt("pick: rock paper or scisors")
+    while (playerSelection == null)
+        prompt("I said pick ROCK, PAPER, SCISORS!!!!!!!!!!")
+return(playerSelection)
+}
 function computerPlay() {
-    return pick[Math.floor(Math.random() * pick.length)]   
+    let choices = ['rock', 'paper', 'scissors']
+    computerChoices = choices[Math.floor(Math.random() * choices.length)]
+return(computerChoices)  
 }
 
-function playerSelection() {
-    chose = prompt('pick: rock, paper or scisors !!')
-}
 
+let playerSelection = playerChoice() 
+let computerSelection = computerPlay()
 
-
-
-function playRound(){
-    let computerSelector = computerPlay
+function playRound(playerSelection , computerSelection) {
     let result = ""
+    let playerScore = ""
+    let computerScore = ""
 
-    if ((computerSelector() == "rock" && playerSelection() == "scisors") ||
-        (computerSelector() == "scisors" && playerSelection() == "paper") ||
-        (computerSelector() == "paper" && playerSelection() == "rock"))
-        {
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
         playerScore += 1
-        alert("you have won !") 
-        }
-        else if (playerSelection == computerSelection) {
-            result = ('It\'s a tie. You both chose ' + playerSelection
-                + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-        }
-        else {
-            computerScore += 1
+        console.log('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "Player score: " + playerScore + "Computer score: " + computerScore)
 
-            if(computerScore == 5) {
-                result = ('computer won!')
-            }
+        if (playerScore == 5) {
+            console.log("You won the game! Reload the page to play again")
         }
+    }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "Player score: " + playerScore + "Computer score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "Player score: " + playerScore + "Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            console.log('I won the game! Reload the page to play again')
+
+        }
+    }
 }
 
-        
+let play = playRound()
+
+
+
+console.log(play)
